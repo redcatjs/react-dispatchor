c29fdfda2d1873770d5aa0652a02bdfa90a6fed2
takion <jo@surikat.pro>
2018-01-22 20:34:44 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export createReducerFromObject from './createReducerFromObject'
 export dispatchorRouterRedux from './dispatchorRouterRedux'
-export createEventDispatcherMiddleWare from './dispatchorRouterRedux'
+export createEventDispatcherMiddleWare from './createEventDispatcherMiddleWare'
 export connectDispatcherToStore from './connectDispatcherToStore'
 export eventDispatcher from './eventDispatcher'
 export default from './eventDispatcher'
