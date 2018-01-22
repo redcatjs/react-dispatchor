2c7f1753f864e3a60b5a93f026ac6f6c97c81501
takion <jo@surikat.pro>
2018-01-22 20:49:17 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import createReducerFromObject from './createReducerFromObject'
-import dispatchorRouterRedux from './dispatchorRouterRedux'
-import createEventDispatcherMiddleWare from './createEventDispatcherMiddleWare'
-import connectDispatcherToStore from './connectDispatcherToStore'
-import eventDispatcher from './eventDispatcher'
-export default eventDispatcher
-export {
-  createReducerFromObject,
-  dispatchorRouterRedux,
-  createEventDispatcherMiddleWare,
-  connectDispatcherToStore,
-  eventDispatcher,
-}
+export createReducerFromObject from './createReducerFromObject'
+export dispatchorRouterRedux from './dispatchorRouterRedux'
+export createEventDispatcherMiddleWare from './createEventDispatcherMiddleWare'
+export connectDispatcherToStore from './connectDispatcherToStore'
+export eventDispatcher from './eventDispatcher'
+export default from './eventDispatcher'
